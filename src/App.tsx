7da04de57096160657a3fe0aa55de1d1a9ac53f3
Kadu <ce.magalhaespereira@gmail.com>
2022-08-06 10:30:47 -0300

    Add basic config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { GlobalStyles } from './styles/GlobalStyles'
+import {ThemeProvider} from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
   return (
-   <h1>Salut !</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Salut !</h1>
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+}
+
+body {
+    font-family: 'Roboto', sans-serif;
+    background-color: ${(props) => props.theme['background']};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+    font-weight: 400;
+    font-size: 1rem;
+}
+`
