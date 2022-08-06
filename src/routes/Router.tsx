a71154769910909d837f848186bec65544fb0f64
Kadu <ce.magalhaespereira@gmail.com>
2022-08-06 11:17:47 -0300

    Add routes and default layout

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 
 // routes
 import { Checkout } from '../pages/Checkout/Checkout'
@@ -8,9 +9,11 @@ import { OrderPlaced } from '../pages/OrderPlaced/OrderPlaced'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/order-placed" element={<OrderPlaced />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-placed" element={<OrderPlaced />} />
+      </Route>
     </Routes>
   )
 }
